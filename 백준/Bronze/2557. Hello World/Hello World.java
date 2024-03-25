5b8bb3a0f8ce661da91a6941e40e3ae932db45fa
dldnr3043 <65563206+dldnr3043@users.noreply.github.com>
2024-03-25 15:56:52 +0900

    [Bronze V] Title: Hello World, Time: 196 ms, Memory: 17444 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello World!");
+    }
+}
